0036cfcfdda4b04b7546033651edc9a1a31970e1
Sujeet Krithik <sujeetkrithik@gmail.com>
2023-04-14 03:08:07 +0530

    initiate the Chat Application

---
+++ Chat-App/src/main/java/com/chat/controller/MessageController.java
@@ -0,0 +1,24 @@
+package com.chat.controller;
+
+import com.chat.model.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @MessageMapping("/message")
+    @SendTo("/topic/return-to")
+    public Message getContent(@RequestBody Message message){
+        try{
+            //processing
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException exception){
+            exception.printStackTrace();
+        }
+        return message;
+    }
+}
